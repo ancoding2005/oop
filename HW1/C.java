b74a4e1509d2385b23666d17074d46f1191df480
Nguyễn Trường An <166909247+ancoding2005@users.noreply.github.com>
2024-04-14 19:02:25 +0700

    Add files via upload

---
+++ HW1/C.java
@@ -0,0 +1,21 @@
+public class C {
+
+    public static class Initials {
+
+        public static void printInitials() {
+            System.out.println(" ***       **    **********            **           ");
+            System.out.println(" ****      **    **********           ****          ");
+            System.out.println(" ** **     **        **              **  **         ");
+            System.out.println(" **  **    **        **             **    **        ");
+            System.out.println(" **   **   **        **            **********       ");
+            System.out.println(" **    **  **        **           ************      ");
+            System.out.println(" **     ** **        **          **          **     ");
+            System.out.println(" **      ****        **         **            **    ");
+            System.out.println(" **       ***        **        **              **   ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Initials.printInitials();
+    }
+}
